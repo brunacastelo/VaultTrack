b6110e68dfe157e629be11a0d22f0652d828e68f
Bruna Castelo <bruna.castelo@jackinthebox.com>
2025-04-03 16:59:07 -0300

    add simple test and update circleci config

---
+++ test/controllers/health.test.js
@@ -0,0 +1,15 @@
+const request = require('supertest');
+const express = require('express');
+const routes = require('../../src/routes');
+
+const app = express();
+app.use('/api', routes);
+
+describe('Health Check API', () => {
+    it('Deve retornar status OK', async () => {
+        const res = await request(app).get('/api/health');
+        expect(res.status).toBe(200);
+        expect(res.body).toHaveProperty('status', 'OK');
+        expect(res.body).toHaveProperty('message', 'API is running smoothly');
+    });
+});
