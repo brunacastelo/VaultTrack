d2b84e02f7fc79049cf32e754e6ab227a9f70bb0
Bruna Castelo <bruna.castelo@jackinthebox.com>
2025-04-03 17:04:31 -0300

    test deploy

--- src/controllers/healthController.js
+++ src/controllers/healthController.js
@@ -1,5 +1,5 @@
 const healthCheck = (req, res) => {
-    res.json({ status: 'OK', message: 'API is running smoothly' });
+    res.json({ status: 'OK', message: 'API is running smoothly!!!' });
 };
 
 module.exports = { healthCheck };
